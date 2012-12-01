f90edcfd7107c2dfe884e1c1fd859196a7380bc6
Justin Hoppensteadt <git@justinhoppensteadt.com>
2012-11-30 17:27:38 -0800

    Clean up

---
+++ awesome/awesome.d/09icons.lua
@@ -0,0 +1,20 @@
+--
+--------------------------------------------------------------------------------
+--         FILE:  09icons.lua
+--        USAGE:  ./09icons.lua 
+--  DESCRIPTION:  sets the icons
+--      OPTIONS:  ---
+-- REQUIREMENTS:  ---
+--         BUGS:  ---
+--        NOTES:  ---
+--       AUTHOR:  Justin Hoppensteadt (JH), <Justin.Hoppensteadt@ticketmaster.com>
+--      COMPANY:  Live Nation
+--      VERSION:  1.0
+--      CREATED:  11/30/2012 03:07:35 PM PST
+--     REVISION:  ---
+--------------------------------------------------------------------------------
+--
+
+function set_tag_icon()
+    os.execute("set-awesome-client-icon.sh")
+end
