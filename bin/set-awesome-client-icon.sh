7085bbbd45695c29c833b2fe187a26124201c86b
Justin Hoppensteadt <git@justinhoppensteadt.com>
2013-05-04 00:47:30 -0700

    Added this external file to update tag icons:

---
+++ bin/set-awesome-client-icon.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  set-awesome-client-icon.sh
+#
+#         USAGE:  ./set-awesome-client-icon.sh
+#
+#   DESCRIPTION:  This should set the icon for the tag to the focused windows icon
+#
+#   mostly
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Justin Hoppensteadt (JH), Justin.Hoppensteadt@ticketmaster.com
+#       COMPANY:  Live Nation
+#       VERSION:  1.0
+#       CREATED:  05/04/2013 12:29:43 AM PDT
+#      REVISION:  ---
+#===============================================================================
+
+( awesome-client < ~/Profile/awesome/tagset.lua ) &
