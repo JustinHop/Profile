799e0ea87212fd2d885acd94d9100702f7a0de55
Justin Hoppensteadt <git@justinhoppensteadt.com>
2013-10-29 01:59:30 -0700

    stashing changes

---
+++ bin/set-awesome-client-icon.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  set-awesome-client-icon.sh
+# 
+#         USAGE:  ./set-awesome-client-icon.sh 
+# 
+#   DESCRIPTION:  sets tag icon to client icon, plus waiting
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Justin Hoppensteadt (JH), Justin.Hoppensteadt@ticketmaster.com
+#       COMPANY:  Live Nation
+#       VERSION:  1.0
+#       CREATED:  11/16/2012 12:25:36 PM PST
+#      REVISION:  ---
+#===============================================================================
+
+sleep .01s
+echo "if client.focus then awful.tag.seticon(client.focus.icon) else awful.tag.seticon() end" | awesome-client &
