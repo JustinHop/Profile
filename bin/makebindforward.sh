6c1115605125038ff7187e7b578bcb98e98bbf1d
Justin Hoppensteadt <git@justinhoppensteadt.com>
2013-12-02 12:03:41 -0800

    Added some tm dns stuff

---
+++ bin/makebindforward.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  makebindforward.sh
+# 
+#         USAGE:  ./makebindforward.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Justin Hoppensteadt (JH), Justin.Hoppensteadt@ticketmaster.com
+#       COMPANY:  Live Nation
+#       VERSION:  1.0
+#       CREATED:  08/28/2013 11:48:45 AM PDT
+#      REVISION:  ---
+#===============================================================================
+
+#!/bin/bash
+rm -f ~/tm.bindForward
+
+for zone in clisys.tmcs cloudsys.tmcs coresys.tmcs fieldsys.tmcs netops.tmcs netsys.tmcs stratsys.tmcs techops.tmcs tm.tmcs tw.tmcs uksys.tmcs vista.tmcs websys.tmcs winsys.tmcs syseng.tmcs
+do
+    dig @iib1.dns.dns2.cloudsys.tmcs $zone axfr >> ~/tm.bindForward
+done
+
+
