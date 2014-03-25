74e470916dc1843c0eb60728e4af9714dac6b70a
Justin Hoppensteadt <git@justinhoppensteadt.com>
2014-03-24 17:50:35 -0700

    dnsflip.py now working forward and reverse

--- python/dnsflip/dnsflip.py
+++ python/dnsflip/dnsflip.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 '''dnsflip
 Usage:
-    dnsflip (--forward|--reverse) [options]
-    dnsflip ([--output-file=LOGFILE] [<FILE>...])
-    dnsflip [--]
-    dnsflip ship <name> move <x> <y> [--speed=<kn>]
-    dnsflip ship shoot <x> <y>
-    dnsflip mine (set|remove) <x> <y> [--moored|--drifting]
+    dnsflip [--forward|--reverse] [FILE...]
     dnsflip -h | --help
     dnsflip --version
 
 Options:
-    -o --output-file=LOGFILE  Output file [default: -].
-    -f --forward              Only preform forward lookups.
+    -f --forward              Only preform forward lookups. [default: True]
     -r --reverse              Only preform reverse lookups.
     -v --version              Show version.
     -h --help                 Show this screen.
@@ -20,6 +14,9 @@ Options:
 
 #from __future__ import unicode_literals, print_function
 from docopt import docopt
+import dns.resolver
+import dns.reversename
+#import string
 import fileinput
 import re
 
@@ -30,28 +27,26 @@ __license__ = "MIT"
 
 class DNSFlip:
 
-    #import socket
-
     '''The DNS Flipper'''
 
     def __init__(self):
         self.ValidIp = re.compile(
-            r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
-            r'(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}')
+            r'((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+            r'(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})')
         self.ValidIpNoArpa = re.compile(
-            r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+            r'((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
             r'(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}'
-            r'(?!(\d+)*\.in-addr\.arpa\.?)')
+            r'(?!(\d+)*\.in-addr\.arpa\.?))')
         self.ValidArpa = re.compile(
-            r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+            r'((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
             r'(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}'
-            r'.in-addr\.arpa\.?')
+            r'.in-addr\.arpa\.?)')
         self.ValidHost = re.compile(
             r'(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)'
             r'*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])')
         self.CommonHost = re.compile(
-            r'(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)'
-            r'*(com|org|net|info|tmcs|pl|tv)\.?', re.IGNORECASE)
+            r'((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)'
+            r'*(com|org|net|info|tmcs|pl|tv)\.?)', re.IGNORECASE)
 
     def strip(self, stringIn):
         self.stripedLine = []
@@ -63,34 +58,73 @@ class DNSFlip:
                         self.stripedLine.append(word)
         return " ".join(self.stripedLine)
 
+    def _resolve_r(self, ipaddr):
+        self.ipv4 = ipaddr.group()
+        #print self.ipv4
+        self.rev = dns.reversename.from_address(self.ipv4)
+        return str(dns.resolver.query(self.rev, "PTR")[0])
+        #return dns.reversename.to_address(self.rev)
+
+    def _resolve(self, hostname):
+        self.host = str(hostname.group())
+        try:
+            self.answers = dns.resolver.query(hostname.group(),
+                                              'A')
+            return self.answers[0].__str__()
+        except dns.resolver.NXDOMAIN:
+            return self.host
+
     def arpa(self, stringIn):
         self.inputString = stringIn
-        return self.ValidArpa.subn("<PTR-RECORD>", self.inputString)[0]
-
-    def forward(self, stringIn):
-        self.inputString = stringIn
-        return self.ValidIpNoArpa.subn("<IP-ADDRESS>", self.inputString)[0]
+        return self.ValidArpa.subn("<PTR-RECORD>",
+                                   self.inputString)[0]
 
     def reverse(self, stringIn):
         self.inputString = stringIn
-        return self.CommonHost.subn("<HOSTNAME>", self.inputString)[0]
+        self._rev = self.ValidIpNoArpa.subn(self._resolve_r,
+                                            self.inputString)[0]
+        #print self._rev
+        return self._rev
+
+    def forward(self, stringIn):
+        self.inputString = stringIn
+        self._rev = self.CommonHost.subn(self._resolve,
+                                         self.inputString)[0]
+        #print self._rev
+        return self._rev
+        # return self.CommonHost.subn("<HOSTNAME>", self.inputString)[0]
+
+    def parse(self, stringIn):
+        self.lineout = []
+        for chunk in re.findall(r'(\s+|\S+)', stringIn):
+            #print "'" + chunk + "'"
+            if (self.CommonHost.match(chunk)):
+                self.lineout.append(self.forward(chunk))
+            elif (self.ValidIpNoArpa.match(chunk)):
+                #print "Is Reverse"
+                self.lineout.append(self.reverse(chunk))
+            else:
+                self.lineout.append(chunk)
+        return "".join(self.lineout)
 
     def flip(self, stringIn):
-        self.inputStringFull = self.arpa(stringIn)
-        self.inputStringFull = self.forward(self.inputStringFull)
+        self.inputStringFull = self.forward(stringIn)
+        #self.inputStringFull = self.forward(self.inputStringFull)
         return self.reverse(self.inputStringFull)
 
 
 def main():
     '''Main entry point for the dnsflip CLI.'''
     args = docopt(__doc__, version=__version__)
-    print(args)
+    #print(args)
     dnsf = DNSFlip()
-    for line in fileinput.input(args['<FILE>']):
+    for line in fileinput.input(args['FILE']):
         line = line.rstrip()
         print (line)
-        print (dnsf.flip(line))
-        print (dnsf.strip(line))
+        print (dnsf.parse(line))
+        if (args['--forward']):
+            print (dnsf.forward(line))
+        #print (dnsf.strip(line))
 
 
 if __name__ == '__main__':
