47d6426f17080869c450f19fccb45decefaf1bf7
Justin Hoppensteadt <git@justinhoppensteadt.com>
2014-05-08 14:33:36 -0700

    getting onetimetsd on tux-ninja

--- python/onetimetsd/onetimetsd.py
+++ python/onetimetsd/onetimetsd.py
@@ -14,6 +14,7 @@ Options:
 '''
 
 from docopt import docopt
+import re
 
 __version__ = "0.1.0"
 __author__ = "Justin Hoppensteadt"
@@ -21,14 +22,28 @@ __license__ = "MIT"
 
 
 class Nsr2TSD:
+    """
+    Does the conversion, expects file opj
+    Need format output like this
 
-    """Does the conversion, expects file opj"""
+    put lb.vserver.mbps <timestamp> <value>
+    loadbalancer=nsr1.sys.ash1.websys.tmcs
+    product=tmol port=80 host=www.ash1.websys.tmcs
+    cluster=ash1 class=pxy
+
+    but without newlines, pep8
+
+    self.data = list of dictionaries
+        \/ list
+        [       { 'timestamp': 1399584406, ... }, {....    ]
+    """
 
     def __init__(self):
         self.args = docopt(__doc__, version=__version__)
         if (self.args['--debug']):
             print ("Nsr2TSD Class Created")
         self.data = {}
+        self.
 
     def add(self, fobj):
         self.f = fobj
