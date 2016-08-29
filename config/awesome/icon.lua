a828ba82336fba1fe7261058c557048c402d06b0
Justin Hoppensteadt <justinrocksmadscience@gmail.com>
2016-08-29 05:56:09 -0700

    Added some scratchfile and a lua exe

---
+++ config/awesome/icon.lua
@@ -0,0 +1,16 @@
+local gears = require("gears")
+local awful = require("awful")
+awful.rules = require("awful.rules")
+require("awful.autofocus")
+-- Widget and layout library
+local wibox = require("wibox")
+-- Theme handling library
+local beautiful = require("beautiful")
+-- Notification library
+local naughty = require("naughty")
+local menubar = require("menubar")
+
+
+--local ico = awful.util.geticonpath(client.focus.name, {"png", "gif", "ico"}, {"/usr/share/icons"})
+
+-- awful.tag.seticon("gnome-mixer.png")
