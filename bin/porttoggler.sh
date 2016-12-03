6359ff18dc2b5c9632e91ef6f2b57afc8477e076
Justin Hoppensteadt <justinrocksmadscience@gmail.com>
2016-12-03 13:18:25 -0800

    port toggler updated

--- bin/porttoggler.sh
+++ bin/porttoggler.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 
 COUNT=0
 INPORT1=6600
@@ -16,12 +15,14 @@ handle_trap () {
 while true ; do
       let "COUNT++"
       notify-send -i /usr/share/icons/HighContrast/48x48/devices/audio-headset.png -a mpd "Toggle MPD Port to $TOPORT1" "Connecting to port $TOPORT1 MPD"
-      nc6 --continuous -l -p $INPORT1 -e "nc6 localhost $TOPORT1" &
+      #nc6 --continuous -l -p $INPORT1 -e "nc6 localhost $TOPORT1" &
+      socat TCP-LISTEN:${INPORT1},fork,reuseaddr TCP:localhost:${TOPORT1} &
       NCPID=$!
       wait $NCPID
       sleep 1s
       notify-send -i /usr/share/icons/HighContrast/48x48/devices/audio-headset.png -a mpd "Toggle MPD Port to $TOPORT2" "Connecting to port $TOPORT2 Mopidy"
-      nc6 --continuous -l -p $INPORT1 -e "nc6 localhost $TOPORT2" &
+      #nc6 --continuous -l -p $INPORT1 -e "nc6 localhost $TOPORT2" &
+      socat TCP-LISTEN:${INPORT1},fork,reuseaddr  TCP:localhost:${TOPORT2} &
       NCPID=$!
       wait $NCPID
       sleep 1s
--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -136,6 +136,7 @@ awful.tag.setnmaster(1, tags[1][2])
 awful.tag.setmwfact(.8, tags[1][2])
 awful.tag.setncol(2, tags[1][2])
 awful.layout.set(awful.layout.suit.max, tags[1][3])
+awful.layout.set(awful.layout.suit.max, tags[1][9])
 -- }}}
 
 -- {{{ Menu
