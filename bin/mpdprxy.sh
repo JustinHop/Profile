b9bc4720380ca31cccf06700705a093eed5059e9
Justin Hoppensteadt <justinrocksmadscience@gmail.com>
2018-03-03 06:46:32 -0800

    Removed python-mode

--- bin/mpdprxy.sh
+++ bin/mpdprxy.sh
@@ -2,7 +2,11 @@
 while true ; do
     ~/go/bin/mpdprxy --port 6600 \
         --hosts localhost:6602,localhost:6601 \
-        --http 6610
+        --http 6610 &
+    _PID=$!
+    sleep 5s
+    curl -v 'http://localhost:6610/?active%5B0%5D=1&default%5B0%5D=1'
+    wait $_PID
     echo FAILED
     sleep 1s;
 done
