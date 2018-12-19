88c85a2e90a10e24d92c9cb7bbd9edfcc69ae7ef
Justin Hoppensteadt <justinrocksmadscience@gmail.com>
2018-12-19 12:33:31 -0800

    trimvids works

--- bin/trimvids.sh
+++ bin/trimvids.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
-#DEV="-i"
+DEV=""
 set -euo pipefail
 IFS=$'\n\t'
 
-
-if [ $1 ]; then
+SAFE=""
+if [ $* ]; then
     SAFE=echo
+    echo dry run
 fi
-SAFE=echo
 
 vidmanage lists
 BEFORE=$(df -h | grep /mnt/auto | while read LINE ; do echo $(echo $LINE| awk 'NF{NF-=1};1'); done )
@@ -29,5 +29,5 @@ done
 
 vidmanage update
 vidmanage lists
-echo $BEFORE | paste - <(df -h | awk '{$1=""}1' | grep /mnt/auto) | awk '{print $1" "$2"/"$6" "$3"/"$7" "$4"/"$8" "$5"/"$9" "$10}' | column -t
+echo $BEFORE | paste - <(df -h | awk '{$1=""}1' | grep /mnt/auto) | awk '{print $1" "$2" "$3"/"$7" "$4"/"$8" "$5"/"$9" "$10}' | sort -k 6| column -t 
 
