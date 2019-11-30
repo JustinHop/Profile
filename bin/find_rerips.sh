bb34f6956013d6a9b7d17423b6b271c9da3a556a
Justin Hoppensteadt <justinrocksmadscience@gmail.com>
2019-12-01 02:22:04 +0700

    Checked out master python-mode

--- bin/find_rerips.sh
+++ bin/find_rerips.sh
@@ -7,19 +7,22 @@ IFS=$'\t\n'
 
 DEBUG=""
 VERBOSE=""
+LS=""
 
-while getopts "Dv" opt; do
-  case ${opt} in
-    D ) DEBUG=1
-        VERBOSE=1
-        set -x
-      ;;
-    v ) VERBOSE=1
-      ;;
-    * ) echo "$0 [-D](DEBUG) [-v](verbose)"
-        exit 1
-      ;;
-  esac
+while getopts "Dvl" opt; do
+    case ${opt} in
+        D ) DEBUG=1
+            VERBOSE=1
+            set -x
+            ;;
+        v ) VERBOSE=1
+            ;;
+        l ) LS=1
+            ;;
+        * ) echo "$0 [-D](DEBUG) [-v](verbose)"
+            exit 1
+            ;;
+    esac
 done
 shift $((OPTIND -1))
 
@@ -31,30 +34,39 @@ function dbg(){
     fi
 }
 
-for RERIP in $(find -L . -type f -size +100M | grep -- '-nv-') ; do
-    dbg "$RERIP"
+function rerips(){
+    for RERIP in $(find -L $VID_DIR -type f -size +100M | grep -- '-nv-') ; do
+        dbg "$RERIP"
 
-    SHORT=$(echo "$RERIP" | grep -oP '^.*(?=[-\.](nv|copy|720|1080).*)')
-    SHORT=$(echo "$SHORT" | perl -pe 's/\W(1080|720)[pP]//')
-    dbg "$SHORT"
+        SHORT=$(echo "$RERIP" | grep -oP '^.*(?=[-\.](nv|copy|720|1080).*)')
+        SHORT=$(echo "$SHORT" | perl -pe 's/\W(1080|720)[pP]//')
+        dbg "$SHORT"
 
-    LINES=""
-    LINENUM=0
+        LINES=""
+        LINENUM=0
 
-    if echo "$RERIP" | grep -sq EDIT ; then
-        EDIT=$(echo "$RERIP" | grep -oP 'EDIT\S\d+')
-        dbg "$EDIT"
-        LINES=$(find -L -type f -wholename "${SHORT}*" | grep $EDIT)
-        dbg $LINES
-    else
-        LINES=$(find -L -type f -wholename "${SHORT}*")
-        dbg $LINES
-    fi
-    LINENUM=$(echo $LINES | xargs -n1 echo | wc -l )
-    dbg $LINENUM
+        if echo "$RERIP" | grep -sq EDIT ; then
+            EDIT=$(echo "$RERIP" | grep -oP 'EDIT\S\d+')
+            dbg "$EDIT"
+            LINES=$(find -L $VID_DIR -type f -wholename "${SHORT}*" | grep $EDIT)
+            dbg $LINES
+        else
+            LINES=$(find -L $VID_DIR -type f -wholename "${SHORT}*")
+            dbg $LINES
+        fi
+        LINENUM=$(echo $LINES | xargs -n1 echo | wc -l )
+        dbg $LINENUM
 
-    if (( $LINENUM > 1 )); then
-        echo $LINES
-    fi
+        if (( $LINENUM > 1 )); then
+            echo $LINES
+        fi
 
-done | xargs -n1 echo | sort -u
+    done | xargs -n1 echo | sort -u
+}
+
+
+if [ "$LS" ]; then
+    rerips | xargs ls -l
+else
+    rerips
+fi
