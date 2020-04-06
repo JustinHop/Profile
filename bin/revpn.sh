1233d768328938e7f06547ff364200c34ea4a569
Justin Hoppensteadt <justinrocksmadscience@gmail.com>
2020-04-06 08:49:39 +0700

    Revpn working

--- bin/revpn.sh
+++ bin/revpn.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-while sleep 30s ; do
-    until ! sleep 30s && ip link show dev tun0 2>&1 > /dev/null ; do
+# set -x
+
+while true ; do
+    if ! ip link show dev tun0 >/dev/null ; then
         if [ -f ~/.config/revpn/enabled ]; then
-            UUID=$(nmcli connection show | grep vpn | grep -P "(US1|VN1|TW1|TH1|SG1|PH1|MY1|MV1|MO1|KR1|JP1|HK1|ID1)" | awk '{ print $2 }' | sort -r | head -n 1)
+            UUID=$(nmcli connection show | grep vpn | grep -P "(US1|VN1|TW1|TH1|SG1|PH1|MY1|MV1|MO1|KR1|JP1|HK1|ID1)" | \
+                grep -oP '([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}){1}' | \
+                sort -R | head -n 1)
             nmcli connection up $UUID
         fi
-    done
+    fi
+    sleep 1m
 done
